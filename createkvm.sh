e7e772e9db6022e3e84c4d84ba1d1f24a32e2a20
Gzqstca0571 <39184787+Gzqstca0571@users.noreply.github.com>
2018-08-27 11:16:27 +0800

    Create createkvm.sh

---
+++ createkvm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "请输入需要创建的虚拟机数量：" num
+read -p "请输入起始的虚拟机编号：node" x
+
+for i in `seq $num`
+do
+cp /root/node.xml /var/lib/libvirt/qemu/node$[i-1+x].xml
+sed -i 's/name>node.*</name>node'$[i-1+x]'</' /var/lib/libvirt/qemu/node$[i-1+x].xml
+sed -i 's/node.*.qcow2/node'$[i-1+x]'.img/' /var/lib/libvirt/qemu/node$[i-1+x].xml
+virsh define /var/lib/libvirt/qemu/node$[i-1+x].xml
+qemu-img create -b /var/lib/libvirt/images/node.qcow2 -f qcow2 /var/lib/libvirt/images/node$[i-1+x].img 20G
+done
