e9353c637ddb77209b8ee774ee48b76fa869b65c
Gzqstca0571 <39184787+Gzqstca0571@users.noreply.github.com>
2018-09-13 23:14:01 +0800

    Update createkvm.sh

--- createkvm.sh
+++ createkvm.sh
@@ -8,5 +8,5 @@ cp /root/node.xml /var/lib/libvirt/qemu/node$[i-1+x].xml
 sed -i 's/name>node.*</name>node'$[i-1+x]'</' /var/lib/libvirt/qemu/node$[i-1+x].xml
 sed -i 's/node.*.qcow2/node'$[i-1+x]'.img/' /var/lib/libvirt/qemu/node$[i-1+x].xml
 virsh define /var/lib/libvirt/qemu/node$[i-1+x].xml
-qemu-img create -b /var/lib/libvirt/images/node.qcow2 -f qcow2 /var/lib/libvirt/images/node$[i-1+x].img 20G
+qemu-img create -b /var/lib/libvirt/images/node.qcow2 -f qcow2 /var/lib/libvirt/images/node$[i-1+x].img
 done
